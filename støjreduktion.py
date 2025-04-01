3f07278645911657888d41c7d7dc50eda8cb1851
Laerkeraaschou <laerke@raaschou.net>
2025-04-01 14:03:50 +0200

    done

--- støjreduktion.py
+++ støjreduktion.py
@@ -43,64 +43,32 @@ def frekvens_domain_filter(data):
             X_final[n] = X_final[n] * 0
     
     transformeret_data = invdft(X_final)
-    
-    plt.figure(figsize=(12, 5))
-    plt.plot(X_final)
-    plt.xlabel("Frekvens (Hz)")
-    plt.ylabel("Amplitude")
-    plt.title("Signal i frekvensdomænet efter lavpas filter (p=100)")
-    plt.grid(True)
-    plt.show()
     return transformeret_data
 
 data = data[0:1900]
 data = data.drop(data.columns[0], axis=1)
 data["id"] = range(1, len(data) + 1)
-
 data.set_index("id", inplace=True)
+
 sound_values = data["sound_level"].values
 sound_values = np.array(sound_values)
-print(sound_values)
-average_data = data["sound_level"].rolling(5).mean()
-print(average_data)
 
-X = np.fft.fft(sound_values)
-freqs = np.fft.fftfreq(len(X), d=1/len(X))
-amplitudes = np.abs(X)
+average_data = data["sound_level"].rolling(11).mean()
+fourier_trans_low_pass = frekvens_domain_filter(sound_values)
 
-cutoff = 100  # fx 30 Hz
-X_filtered = X.copy()
-X_filtered[np.abs(freqs) > cutoff] = 0
+data["sound_level_smooth"] = average_data
+data["low_pass"] = fourier_trans_low_pass
 
-filtered_signal = np.fft.ifft(X_filtered).real
-
-efter_transformation = frekvens_domain_filter(sound_values)
-
-
-
-#nyt_data = frekvens_domain_filter(sound_values)
-#df = pd.DataFrame(nyt_data, columns=["sound_levelT"])
-
-data["sound_level_smooth"] = data["sound_level"].rolling(window=11).mean()
-data["efter_frekvens"] = X
 # Step 4: Plot one or more variables
 plt.figure(figsize=(12, 5))
-#plt.plot(data.index, data["accel_x"], label="X")
-#plt.plot(data.index, data["accel_y"], label="Y")
-#plt.plot(data.index, data["accel_z"], label="Z")
-#plt.plot(data.index, data["sound_level"], label="Original sound")
-plt.plot(data.index, data["efter_frekvens"], label="Low pass filter")
+
+plt.plot(data.index, data["sound_level"], label="Original sound")
+plt.plot(data.index, data["low_pass"], label="Low pass filter")
+
 plt.xlabel("Time")
 plt.ylabel("Sound levels")
 plt.title("Time series plot: Sound levels")
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
-#plt.show()
-
-# Save to working directory
-plt.savefig('transformation.jpg')
-plt.close()
-
-
-# vi måler nu 2 ms mellem målinger = 1/0.002 = 500 Hz og så skal vi omregne frekvens på det dobbelte = 1000
+plt.show()
